bf8ec76be8df1437d665a64899881b3744fcd0f5
Ngoc Tu <buingoctu1998@gmail.com>
2019-08-13 14:22:41 +0700

    finalization player jump

--- Assets/Scripts/PlayScene/PlayerScript.cs
+++ Assets/Scripts/PlayScene/PlayerScript.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Timers;
 
 public class PlayerScript : MonoBehaviour
 {
     private ControllerScript controllerScript;
-    public float speed;
-    public float jumpForce;
+    public float speed= 10;
+    public float jumpForce = 200f;
     bool isJumping;
     Rigidbody2D rb;
+    private float lasttime;
+    float curtime;
 
     private void Start()
     {
@@ -22,11 +25,23 @@ public class PlayerScript : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        // will be modified whenever hit the side of another block instead of top and btm of block
         controllerScript.changeDirection();
     }
 
+    private IEnumerator Coroutine()
+    {
+        yield return new WaitForSeconds(0.3f);        
+        GetComponent<Rigidbody2D>().gravityScale *= -1;
+
+    }
     public void Jump()
     {
         Debug.Log("jumping");
+        GetComponent<Rigidbody2D>().gravityScale *= -1;
+        StartCoroutine(Coroutine());
+        
+
+
     }
 }
