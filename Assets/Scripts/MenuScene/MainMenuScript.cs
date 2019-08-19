cb6c3cd71a162d3161542256b317cca5a0570bee
Ngoc Tu <buingoctu1998@gmail.com>
2019-08-19 15:26:48 +0700

    fix bug

---
+++ Assets/Scripts/MenuScene/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Button quitBtn;
+    public GameObject mainMenu;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level Scene");
+    }
+
+    public void QuitGame()
+    {
+        //Application.Quit();
+    }
+
+    
+}
